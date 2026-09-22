bfd5008579dd58727ace94617e35f43366f74d9d
ayman1417 <a741003@gmail.com>
2026-09-22 16:45:55 +0300

    update sidebar

--- app/components/sidebar.jsx
+++ app/components/sidebar.jsx
@@ -4,6 +4,7 @@ import React from 'react'
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase/firebase.config";
 import { useRouter } from "next/navigation";
+import { ChartColumnIncreasingIcon, LayoutGrid, LogOut, TableOfContents } from 'lucide-react';
 
 export default function Sidebar() {
 
@@ -16,16 +17,27 @@ export default function Sidebar() {
 
     }
     return (
-        <aside className="bg-linear-to-r  md:w-[220px] px-2 fixed top-0 left-0 z-50 h-dvh from-secondary to-blue-950 text-gray-200 flex flex-col items-center  md:items-start p-5 gap-5 -h-dvh col-span-1 w-[65px]
+        <aside className="bg-linear-to-r  md:w-[220px] px-2 fixed top-0 left-0 z-50 h-dvh from-secondary to-blue-950 text-gray-200 flex flex-col items-center  md:items-start p-5 gap-5 -h-dvh col-span-1 w-[50px]
           max-md:items-center
           max-md:px-2">
-            <h1 className="text-xs md:text-lg md:block">Main Dashbord</h1>
+            <h1 className=" hidden text-xs md:text-lg md:block">My Dashbord</h1>
             <div className="flex flex-col justify-between h-full items-start  p-2 gap-5">
                 <div className="flex flex-col items-start p-2 gap-5">
-                    <Link className="text-xs md:text-lg" href={"/"}>Products</Link>
-                    <Link className="text-xs md:text-lg" href={"/statistics"}>statistics</Link>
+                    <Link className="text-xs md:text-lg flex items-center gap-1" href={"/"}>
+                        <LayoutGrid />
+                        <span className='hidden md:inline ' > Products </span>
+                    </Link>
+                    <Link className="text-xs md:text-lg flex items-center gap-1" href={"/statistics"}>
+                        <ChartColumnIncreasingIcon />
+                        <span className='hidden md:inline '>statistics</span>
+                    </Link>
                 </div>
-                <button onClick={Logout} className=" text-center md:mb-16 ml-2  text-xs md:text-md mt-[100%] bg-red-500 hover:bg-red-600 duration-200  font-semibold text-white p-2 rounded-md cursor-pointer" >Log Out</button>
+                {/* <div className="flex gap-1"> */}
+                <button onClick={Logout} className=" text-center md:mb-16 ml-2 flex items-center gap-1  text-xs md:text-md mt-[100%] bg-red-500 hover:bg-red-600 duration-200  font-semibold text-white p-2 rounded-md cursor-pointer" >
+                    <LogOut size={15} />
+                    <span className='hidden md:inline'>Logout</span>
+                </button>
+                {/* </div> */}
                 {/* <Link className="text-xs md:text-lg" href={"/signup"}>SignUp</Link> */}
             </div>
         </aside>
